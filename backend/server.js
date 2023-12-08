2ea499393b7e18519344e64e856192ac844d6382
Kendall Crockett <kwc9mb@missouri.edu>
2023-12-07 23:12:29 -0600

    Update MongoDB connection URI

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 
 // Load environment variables
-dotenv.config();
+//dotenv.config();
 
 // Import routes
 const marketRoutes = require('./routes/market');
@@ -16,7 +16,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+// Define the MongoDB connection URI
+const MONGO_URI = 'mongodb+srv://doadmin:hW245rf873p9O6oE@db-mongodb-nyc3-82778-f53ef0a2.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-nyc3-82778&tls=true';
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
